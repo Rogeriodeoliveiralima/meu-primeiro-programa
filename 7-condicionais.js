4c81cb06af4aaa56c5faff81ece6b3af0c6ab4f7
Rogerio Lima <rlimapg@gmail.com>
2024-04-20 20:02:20 -0300

    encadeamento if

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,30 +1,32 @@
 console.log('Trabalhando com condicionais');
-const idadeComprador = 13;
+const idadeComprador = 33;
+const estaAcompanhada = false;
+const passagemComprada = true;
 
 const listaDeDestino = new Array ('praiGrande', 'santos','saoVicente','cubatao');
 
 console.log(listaDeDestino);
 
-if(idadeComprador >= 18){
+if(idadeComprador >= 18 || estaAcompanhada == true){
     listaDeDestino.splice(2,1);
     console.log ('Comprador maior de Idade');
-}else{
-    console.log('Não e maior de idade, Não posso vender a passagem')
-}
-
-
-
-//remover
-
-console.log(listaDeDestino);
-
-
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idadade");
-    listaDeDestinos.splice(1, 1); // removendo item
-}   else if (estaAcompanhada == true) {
-    console.log("Comprador está acompanhado");
-    listaDeDestinos.splice(1, 1); //removedo item
+    
 }else {
-    console.log("Não é maior de idade e não posso vender");
+    console.log(listaDeDestino);
+    console.log('Não e maior de idade, Não posso vender a passagem');
 }
+
+console.log('Embarque: \n\n');
+
+if(idadeComprador >= 18 && passagemComprada ){
+    console.log('Boa Viagem');
+}else{
+    console.log('Voce nao pode embarcar');
+}
+   
+
+
+    
+
+
+
